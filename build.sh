9cd95caf1de3a2065212e251b1e10b15fc773935
GabrielCogne <gabriel.cogne@gmail.com>
2022-09-22 18:53:03 +0200

    Setup dev environment

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker build --target development -t micro-restaurant/bff -f ./bff/Dockerfile ./bff
+
+docker build --target development -t micro-restaurant/self-service-kiosk-bff -f ./self-service-kiosk/Dockerfile ./self-service-kiosk
+
+docker build --target development -t micro-restaurant/self-service-kiosk -f ./self-service-kiosk/Dockerfile ./self-service-kiosk
+
+# Build backend
+cd ./backend_nestjs
+./build-all.sh
+cd ..
