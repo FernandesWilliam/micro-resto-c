1153a89ba0d6d823fc8f109ecf4da4c0be0ac0cc
GabrielCogne <gabriel.cogne@gmail.com>
2022-09-22 18:53:03 +0200

    Setup dev environment

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker-compose --env-file ./.env.docker \
+               --file ./docker-compose.yml \
+               --file ./backend_nestjs/dining-service/docker-compose-dining.yml \
+               --file ./backend_nestjs/kitchen-service/docker-compose-kitchen.yml \
+               --file ./backend_nestjs/gateway/docker-compose-gateway.yml \
+               --file ./backend_nestjs/menu-service/docker-compose-menu.yml up
