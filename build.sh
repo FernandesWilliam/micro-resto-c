a3d73666158c3c5c833a381ce54cdf28a976d087
GabrielCogne <gabriel.cogne@gmail.com>
2022-09-26 10:24:34 +0200

    Add download dependencies

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+function dlDependencies(){ # $1 contains the directory path
+  cd $1
+  npm ci
+  cd ..
+}
+
+dlDependencies bff
 docker build --target development -t micro-restaurant/bff -f ./bff/Dockerfile ./bff
 
+dlDependencies self-service-kiosk
 docker build --target development -t micro-restaurant/self-service-kiosk-bff -f ./self-service-kiosk/Dockerfile ./self-service-kiosk
 
 docker build --target development -t micro-restaurant/self-service-kiosk -f ./self-service-kiosk/Dockerfile ./self-service-kiosk
