3802445e24beeed18d8182791bc7bfba17de3c3a
GabrielCogne <gabriel.cogne@gmail.com>
2022-10-15 21:38:47 +0200

    Create demo-cmd.sh

---
+++ demo-cmd.sh
@@ -0,0 +1,34 @@
+GATEWAY="http://localhost:9500"
+KITCHEN="$GATEWAY/kitchen"
+DINING="$GATEWAY/dining"
+
+prepareItem() {
+    shortName=$(curl -s -S -X POST "$KITCHEN/preparedItems/$1/start" | grep -o '"shortName":"[^"]*' | grep -o '[^"]*' | tail -1)
+    echo "Start cooking ${shortName^}"
+
+    shortName=$(curl -s -S -X POST "$KITCHEN/preparedItems/$1/finish" | grep -o '"shortName":"[^"]*' | grep -o '[^"]*' | tail -1)
+    echo "Finish cooking ${shortName^}"
+}
+
+prepareItems() {
+    for itemId in $@
+    do
+        prepareItem $itemId
+    done
+}
+
+deliverPreparations() {
+    for id in $@
+    do
+        echo "$id"
+        res=$(curl -s -S -X POST "$KITCHEN/preparations/$id/takenToTable")
+        echo "$res"
+    done
+}
+
+payForOrder() {
+    tableNumber=$(curl -s -S -X POST "$DINING/tableOrders/$1/bill" | grep -o '"tableNumber":[^,]*' | grep -o '[^:,]*' | tail -1)
+    echo "Order $tableNumber payed"
+}
+
+"$@"
