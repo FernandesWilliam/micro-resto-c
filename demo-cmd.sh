3d297bea5abc6d4508c295b99ec6ed0f22632dae
GabrielCogne <gabriel.cogne@gmail.com>
2022-11-22 09:55:47 +0100

    Update demo-cmd.sh

--- demo-cmd.sh
+++ demo-cmd.sh
@@ -2,6 +2,30 @@ GATEWAY="http://localhost:9500"
 KITCHEN="$GATEWAY/kitchen"
 DINING="$GATEWAY/dining"
 
+startItem() {
+    shortName=$(curl -s -S -X POST "$KITCHEN/preparedItems/$1/start" | grep -o '"shortName":"[^"]*' | grep -o '[^"]*' | tail -1)
+    echo "Start cooking ${shortName^}"
+}
+
+finishItem() {
+    shortName=$(curl -s -S -X POST "$KITCHEN/preparedItems/$1/finish" | grep -o '"shortName":"[^"]*' | grep -o '[^"]*' | tail -1)
+    echo "Finish cooking ${shortName^}"
+}
+
+startItems() {
+    for itemId in $@
+    do
+        startItem $itemId
+    done
+}
+
+finishItems() {
+    for itemId in $@
+    do
+        finishItem $itemId
+    done
+}
+
 prepareItem() {
     shortName=$(curl -s -S -X POST "$KITCHEN/preparedItems/$1/start" | grep -o '"shortName":"[^"]*' | grep -o '[^"]*' | tail -1)
     echo "Start cooking ${shortName^}"
